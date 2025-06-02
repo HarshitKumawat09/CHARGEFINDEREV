9685bd9fecc2dd2ceab6e126fd3cbd8036f2f48e
HarshitKumawat09 <harshitkumawat0910@gmail.com>
2025-06-02 18:45:21 +0530

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,6 @@
 const config = {
     backendUrl: "http://localhost:5000/api/stations", // Backend API endpoint
-    googleMapsApiKey: "AIzaSyAlClHQ6nSz0cM2_2kWIaLpM-MxP1R7Auc",    // Google Maps API key
+    googleMapsApiKey: "your api key",    // Google Maps API key
     defaultLocation: { lat: 26.9124, lng: 75.7873 }, // Centered on Jaipur, Rajasthan
 
     endpoints: {
